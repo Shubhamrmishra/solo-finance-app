c533750d1fbdfd720cae5b5a8e6fd3c5ea04e105
Shubham Mishra <shubhamrmishra999@gmail.com>
2026-05-21 18:44:41 +0530

    feature/jwt-authentication  : Added Login API, JWT generation and JWT validation

---
+++ src/main/java/com/solo_finance_app/auth/dto/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.solo_finance_app.auth.dto;
+
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/solo_finance_app/security/JwtService.java
@@ -0,0 +1,35 @@
+package com.solo_finance_app.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY =
+            "myverysecuresecretkeymyverysecuresecretkey123456";
+
+    private Key getSignKey() {
+        return Keys.hmacShaKeyFor(
+                SECRET_KEY.getBytes(StandardCharsets.UTF_8)
+        );
+    }
+
+    public String generateToken(String email) {
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(
+                        new Date(System.currentTimeMillis() + 1000 * 60 * 60)
+                )
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
