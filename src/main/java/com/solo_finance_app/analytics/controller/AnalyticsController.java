2500427f57e9cb2038cdd6237e56b993f9de7c96
Shubham Mishra <shubhamrmishra999@gmail.com>
2026-05-21 20:20:48 +0530

    dev: Implemented Dashboard analytics and Category breakdown

--- src/main/java/com/solo_finance_app/analytics/controller/AnalyticsController.java
+++ src/main/java/com/solo_finance_app/analytics/controller/AnalyticsController.java
@@ -1,10 +1,12 @@
 package com.solo_finance_app.analytics.controller;
 
+import com.solo_finance_app.analytics.dt.CategoryExpenseResponse;
 import com.solo_finance_app.analytics.dt.MonthlySummaryResponse;
-import com.solo_finance_app.analytics.service.
-        AnalyticsService;
+import com.solo_finance_app.analytics.service.AnalyticsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/analytics")
@@ -14,11 +16,13 @@ public class AnalyticsController {
     private final AnalyticsService analyticsService;
 
     @GetMapping("/monthly-summary")
-    public MonthlySummaryResponse getMonthlySummary(
-            @RequestParam Double income
-    ) {
+    public MonthlySummaryResponse getMonthlySummary(@RequestParam Double income) {
+        return analyticsService.getMonthlySummary(income);
+    }
 
-        return analyticsService
-                .getMonthlySummary(income);
+    @GetMapping("/category-wise")
+    public List<CategoryExpenseResponse> getCategoryWiseExpenses() {
+
+        return analyticsService.getCategoryWiseExpenses();
     }
 }
--- src/main/java/com/solo_finance_app/expense/repository/ExpenseRepository.java
+++ src/main/java/com/solo_finance_app/expense/repository/ExpenseRepository.java
@@ -3,6 +3,7 @@ package com.solo_finance_app.expense.repository;
 import com.solo_finance_app.expense.entity.Expense;
 import com.solo_finance_app.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -12,9 +13,13 @@ public interface ExpenseRepository
 
     List<Expense> findByUser(User user);
 
-    List<Expense> findByUserAndExpenseDateBetween(
-            User user,
-            LocalDate startDate,
-            LocalDate endDate
-    );
+    List<Expense> findByUserAndExpenseDateBetween(User user, LocalDate startDate, LocalDate endDate);
+
+    @Query("""
+       SELECT e.category, SUM(e.amount)
+       FROM Expense e
+       WHERE e.user = :user
+       GROUP BY e.category
+       """)
+    List<Object[]> getCategoryWiseExpenses(User user);
 }
